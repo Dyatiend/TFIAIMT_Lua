defc43baa42beb1a5f4bfa56fa4e27ff7ab1c011
Moskalenko Nikita <53981215+Strupervlg@users.noreply.github.com>
2022-11-14 12:11:36 +0300

    Реализован запуск парсера в джаве

--- Compiler/src/main/kotlin/Main.kt
+++ Compiler/src/main/kotlin/Main.kt
@@ -2,14 +2,17 @@ import nodes.Utils
 import java.io.File
 
 fun main(args: Array<String>) {
-    val rootNode = Utils.fromXML("../LuaCompiler/cmake-build-debug/xml.xml")!!
+
+    val process = ProcessBuilder("../LuaCompiler/cmake-build-debug/LuaCompiler","../LuaCompiler/tests/test10.lua").start()
+
+    val rootNode = Utils.fromXML("xml.xml")!!
 
     fillTables(rootNode)
 
     constantsTable.toCSV()
     localVarsTable.toCSV()
 
-    val file = File("tree.dot")
+    val file = File("./javaDot/tree.dot")
     file.writeText("")
     Utils.printProgram(rootNode, file)
     file.writeText(file.readText().replace("@", "").replace("nodes.", ""))
